51b074b9823ce5f41d967a6069a703d366d8f6db
Marc Takoumba <mtakoumba@gmail.com>
2024-11-03 21:14:05 +0100

    utilisation de React Hook Form pour la recuperation des données grace a register

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,9 +1,13 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -13,6 +17,8 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm(!showForm)}>Add new cabin</Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
